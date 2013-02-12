91979ce842b5711c0b41cbe446d17f576d0055f7
Sami Niiranen <zamzum@gmail.com>
2013-02-12 16:02:30 +0200

    started sqlite implementation

--- gfrate/main/rest/GfrateService.py
+++ gfrate/main/rest/GfrateService.py
@@ -1,14 +1,39 @@
 '''
 Created on Feb 5, 2013
 
-@author: sami
+@author: sailniir
 '''
+
+import sqlite3
 from flask import Flask
+from contextlib import closing
+
+# configuration
+DATABASE = '/tmp/gfrate_test.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Here be index!'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return 'Hello {0}'.format(name)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

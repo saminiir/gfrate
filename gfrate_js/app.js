32dbdb554fe4042884d62d008b23133a4d0d582d
Sami Niiranen <sami.i.niiranen@student.jyu.fi>
2013-03-07 21:13:17 +0200

    Heroku iter3

--- gfrate_js/app.js
+++ gfrate_js/app.js
@@ -1,36 +1,13 @@
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
 
 var app = express.createServer(express.logger());
-var port = process.env.PORT || 5000;
 
-app.configure(function(){
-  app.set('port', port); 
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-app.listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });

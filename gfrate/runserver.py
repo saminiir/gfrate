aecfe8a9b5ff358354984dd0087684f419cbc1ff
Sami Niiranen <sami.i.niiranen@student.jyu.fi>
2013-03-07 20:08:16 +0200

    Removed Python and PHP project stubs

--- gfrate/runserver.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on Feb 18, 2013
-
-@author: sailniir
-'''
-from gfrate.main import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- gfrate_php/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'Slim/Slim.php';
-\Slim\Slim::registerAutoloader();
-
-?>

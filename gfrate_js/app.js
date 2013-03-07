2b4e248d6fd59ce355ee34605dd911860c1dd107
Sami Niiranen <sami.i.niiranen@student.jyu.fi>
2013-03-07 23:40:33 +0200

    Added oauthorize & passport to project

--- gfrate_js/app.js
+++ gfrate_js/app.js
@@ -1,11 +1,26 @@
-
+var oauthorize = require('oauthorize');
 var express = require('express');
+var passport = require('passport');
 
 var app = express();
+var server = oauthorize.createServer();
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
+
+app.post('/request_token',
+  passport.authenticate('consumer', { session: false }),
+  server.requestToken(function(client, callbackURL, done) {
+    var token = utils.uid(8)
+      , secret = utils.uid(32)
+
+    var t = new RequestToken(token, secret, client.id, callbackURL);
+    t.save(function(err) {
+      if (err) { return done(err); }
+      return done(null, token, secret);
+    });
+  }));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {

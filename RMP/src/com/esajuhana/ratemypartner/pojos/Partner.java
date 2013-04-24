c95ab5c891fdac476a7430146b53b745fb813e40
esa.j.lahikainen <esa.j.lahikainen@student.jyu.fi>
2013-04-24 18:36:06 +0300

    Hall of Fame activity started, only dummy data, icons added

---
+++ RMP/src/com/esajuhana/ratemypartner/pojos/Partner.java
@@ -0,0 +1,28 @@
+package com.esajuhana.ratemypartner.pojos;
+
+/**
+ * Not in use
+ * @author munkpad
+ *
+ */
+public class Partner {
+	
+	private String partnerRank;
+	private String partnerDescription;
+	//private int partnerPositive;
+	//private int partnerNegative;
+	
+	public String getPartnerRank() {
+		return partnerRank;
+	}
+	public void setPartnerRank(String partnerRank) {
+		this.partnerRank = partnerRank;
+	}
+	public String getPartnerDescription() {
+		return partnerDescription;
+	}
+	public void setPartnerDescription(String partnerDescription) {
+		this.partnerDescription = partnerDescription;
+	}
+
+}
